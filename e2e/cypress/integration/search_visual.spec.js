9822a3b08a35a412cd4130112308b24e87696ecf
glendy-zulay <92461881+glendy-zulay@users.noreply.github.com>
2022-09-02 09:26:00 -0500

    Deleted one test

--- e2e/cypress/integration/search_visual.spec.js
+++ e2e/cypress/integration/search_visual.spec.js
@@ -10,14 +10,5 @@ describe('Search for books - eyes', () => {
       cy.get('#searchBar').type('Agile Testing', {delay:250})
       cy.eyesCheckWindow('Agile Testing')
       cy.eyesClose()
-    })
-  
-    it('eyes should return multiple books with title Test', () => {
-      cy.eyesOpen({
-        appName: 'Automation Bookstore'
-      })
-      cy.get('#searchBar').type('Test', {delay:250})
-      cy.eyesCheckWindow('Test')
-      cy.eyesClose()
-    })
+    })   
 })
