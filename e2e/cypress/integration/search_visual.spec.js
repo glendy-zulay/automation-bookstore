1002164e1637ffc26e45d56fa162154095a378db
glendy-zulay <92461881+glendy-zulay@users.noreply.github.com>
2022-09-02 09:05:34 -0500

    deleted appName in eyesOpen

--- e2e/cypress/integration/search_visual.spec.js
+++ e2e/cypress/integration/search_visual.spec.js
@@ -4,18 +4,14 @@ describe('Search for books - eyes', () => {
     })  
   
     it('eyes should return one book with title Agile Testing', () => {
-      cy.eyesOpen({
-        appName: 'Automation Bookstore Demo Glendy Duran'        
-      })
+      cy.eyesOpen()
       cy.get('#searchBar').type('Agile Testing', {delay:250})
       cy.eyesCheckWindow('Agile Testing')
       cy.eyesClose()
     })
   
     it('eyes should return multiple books with title Test', () => {
-      cy.eyesOpen({
-        appName: 'Automation Bookstore Demo Glendy Duran'        
-      })
+      cy.eyesOpen()
       cy.get('#searchBar').type('Test', {delay:250})
       cy.eyesCheckWindow('Test')
       cy.eyesClose()
